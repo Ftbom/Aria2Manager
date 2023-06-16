d136d2c75837b99b2b4e0371358a99d3ef4f00c3
Ftbom <lz490070@gmail.com>
2023-06-16 22:24:11 +0800

    显示下载条目

--- Models/DownloadItemModel.cs
+++ Models/DownloadItemModel.cs
@@ -8,17 +8,18 @@ namespace Aria2Manager.Models
 {
     public class DownloadItemModel
     {
+        public string Gid { get; set; }
         public string Name { get; set; }
         public string Size { get; set; }
-        public int Progress { get; set; }
+        public double Progress { get; set; }
         public string ETA { get; set; }
         public string Status { get; set; }
         public string UploadSpeed { get; set; }
         public string DownloadSpeed { get; set; }
         public string Uploaded { get; set; }
         public string Downloaded { get; set; }
-        public double Ratio { get; set; }
-        public int Connections { get; set; }
-        public int Seeds { get; set; }
+        public double? Ratio { get; set; }
+        public long Connections { get; set; }
+        public long? Seeds { get; set; }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Aria2Manager.Models;
@@ -50,6 +50,7 @@ namespace Aria2Manager.ViewModels
             //TODO:判断服务器可连接状态
             server_connected = true;
             DownloadItems = new ObservableCollection<DownloadItemModel>();
+            UpdataDownloadItems();
         }
 
         private void Exit(object? parameter)
@@ -63,7 +64,7 @@ namespace Aria2Manager.ViewModels
         {
             try
             {
-                System.Diagnostics.Process.Start(new ProcessStartInfo("https://aria2.github.io"){ UseShellExecute = true });
+                Process.Start(new ProcessStartInfo("https://aria2.github.io"){ UseShellExecute = true });
             }
             catch (System.ComponentModel.Win32Exception noBrowser)
             {
@@ -77,6 +78,100 @@ namespace Aria2Manager.ViewModels
             {
                 MessageBox.Show(other.Message);
             }
+        }
+
+        private async void UpdataDownloadItems()
+        {
+            while (true)
+            {
+                try
+                {
+                    var client = new Aria2ClientModel(Aria2Server);
+                    var Items = await client.Aria2Client.TellAllAsync();
+                    long total_download_speed = 0;
+                    long total_upload_speed = 0;
+                    DownloadItems.Clear();
+                    foreach (var item in Items)
+                    {
+                        DownloadItemModel download_item = new DownloadItemModel();
+                        download_item.Gid = item.Gid;
+                        if ((item.Bittorrent == null) || (item.Bittorrent.Info == null))
+                        {
+                            download_item.Name = Path.GetFileName(item.Files[0].Path);
+                        }
+                        else
+                        {
+                            download_item.Name = item.Bittorrent.Info.Name;
+                        }
+                        download_item.Size = BytesToString(item.TotalLength);
+                        download_item.Progress = (double)(item.CompletedLength * 10000 / item.TotalLength) / 100;
+                        download_item.Status = item.Status;
+                        download_item.Downloaded = BytesToString(item.CompletedLength);
+                        download_item.Uploaded = BytesToString(item.UploadLength);
+                        download_item.DownloadSpeed = BytesToString(item.DownloadSpeed) + "/s";
+                        download_item.UploadSpeed = BytesToString(item.UploadSpeed) + "/s";
+                        total_download_speed += item.DownloadSpeed;
+                        total_upload_speed += item.UploadSpeed;
+                        if (item.DownloadSpeed == 0)
+                        {
+                            download_item.ETA = "--";
+                        }
+                        else
+                        {
+                            download_item.ETA = SecondsToString((int)((double)(item.TotalLength - item.CompletedLength) / item.DownloadSpeed));
+                        }
+                        if (item.CompletedLength == 0)
+                        {
+                            download_item.Ratio = null;
+                        }
+                        else
+                        {
+                            download_item.Ratio = (double)(item.UploadLength * 100 / item.CompletedLength) / 100;
+                        }
+                        download_item.Connections = item.Connections;
+                        download_item.Seeds = item.NumSeeders;
+                        DownloadItems.Add(download_item);
+                    }
+                    //UploadSpeed = BytesToString(total_upload_speed) + "/s";
+                    //DownloadSpeed = BytesToString(total_download_speed) + "/s";
+                }
+                catch { }
+                await Task.Delay(500);
+            }
+        }
+
+        private string BytesToString(long byteCount)
+        {
+            long GBSize = 1024 * 1024 * 1024;
+            long MBSize = 1024 * 1024;
+            long KBSize = 1024;
+            if (byteCount >= GBSize)
+            {
+                return ((double)(byteCount * 100 / GBSize) / 100).ToString() + "GB";
+            }
+            else if (byteCount >= MBSize)
+            {
+                return ((double)(byteCount * 100 / MBSize) / 100).ToString() + "MB";
+            }
+            else
+            {
+                return ((double)(byteCount * 100 / KBSize) / 100).ToString() + "KB";
+            }
+        }
+
+        private string SecondsToString(long secCount)
+        {
+            string result = "";
+            long HSize = 60 * 60;
+            long MSize = 60;
+            int h_num = (int)(secCount / HSize);
+            result += h_num.ToString() + "h";
+            secCount -= h_num * HSize;
+            int m_num = (int)(secCount / MSize);
+            result += m_num.ToString() + "m";
+            secCount -= m_num * MSize;
+            result += secCount.ToString() + "s";
+            return result;
         }
     }
 }
